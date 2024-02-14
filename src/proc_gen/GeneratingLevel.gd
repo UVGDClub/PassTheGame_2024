6ddfed4438643e8822b0295982c3a88c8000345c
Avery Simmons <eno116116@gmail.com>
2024-02-13 21:20:23 -0800

    I changed one value

--- src/proc_gen/GeneratingLevel.gd
+++ src/proc_gen/GeneratingLevel.gd
@@ -5,7 +5,7 @@ extends Node2D
 @onready var player = $Player
 
 const ROOM_SIZE = Vector2(38, 20)
-const HALLWAY_LENGTH = Vector2(15, 15)
+const HALLWAY_LENGTH = Vector2(20, 15)
 
 const START_RECT_SIZE = Vector2(10, 10)
 
@@ -13,7 +13,7 @@ const EXTRA_RECT_NUM = 4
 const EXTRA_RECT_SIZE_MIN = Vector2(10, 10)
 const EXTRA_RECT_SIZE_MAX = Vector2(19, 10)
 
-const MAX_ROOMS = 200
+const MAX_ROOMS = 20
 
 const CELL_SIZE = ROOM_SIZE + HALLWAY_LENGTH
 
