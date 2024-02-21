c44071753cf447189c5794147878255c7e6734c7
Zadius5 <kytrieu526@gmail.com>
2024-02-21 14:10:59 -0800

    Revert "Revert "Basic enemies""
    This reverts commit f1fde61c3e847bb76db18994a88d8b6eb7a8de1d.

---
+++ src/enemies/enemies.gd
@@ -0,0 +1,29 @@
+class_name Enemies extends Node
+
+
+@onready var pick_up = preload("res://src/cards/card_pack/card_pack_pickup.tscn")
+@export var health: float
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if health <= 0:
+		handle_death()
+
+
+	
+func handle_death():
+	spawn_card()
+
+func spawn_card():
+	var new_pick_up = pick_up.instantiate()
+	new_pick_up.global_position = self.global_position
+	get_parent().add_child(new_pick_up)
+	queue_free()
+
+func _on_hurtbox_area_entered(area):
+	health -= area.hit_damage
+	print("damaged" + str(area.hit_damage))
